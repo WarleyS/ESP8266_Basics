cb4a49f7dac14516ec62c8a81d32b8c6703f1680
WarleyS <warleyncs@gmail.com>
2021-06-26 19:35:37 -0300

    Update ESP_DHT11_Basic.ino

--- ESP_DHT11_Basic/ESP_DHT11_Basic.ino
+++ ESP_DHT11_Basic/ESP_DHT11_Basic.ino
@@ -7,12 +7,22 @@
 
 //Inicializa sensor
 DHTesp dht;
+    
+  float humid = dht.getHumidity();
+  float temp = dht.getTemperature();
+  int humidTone = dht.getHumidity();
 
 void setup() {
+  //DHT
   Serial.begin(115200); // Velocidade - Serial Monitor
   String thisBoard= ARDUINO_BOARD;
   Serial.println(thisBoard);
   dht.setup(16, DHTesp::DHT11); // Connect DHT sensor to GPIO16 - D0 -- Change according the sensor: DHT11 or DHT22
+
+  //Tone Output Pin
+  pinMode(5, OUTPUT);
+
+
 }
 
 void dhtData(){
@@ -21,15 +31,25 @@ void dhtData(){
   float temp = dht.getTemperature();
   Serial.print(dht.getStatusString());
   Serial.print("\n");
-  Serial.print("Umidade:  ");
+  Serial.print("Umidade: ");
   Serial.print(humid, 1);
-  Serial.print("\t\t");
-  Serial.print("Temperatura:  ");
+  Serial.print("\t");
+  Serial.print("Temperatura: ");
   Serial.print(temp, 1);
-  Serial.print("\t\t");
+   Serial.print("\t");
+  Serial.print("Tone: ");
+  Serial.print(humidTone);
+  Serial.print("\t");
   delay(1000);
 }
 
+void tempTone(){
+    tone(5,440,500);
+
+}
+
 void loop() {
+
   dhtData();
+  tempTone();
 }
